fb33906224f458e4ee4d9377350c4eaa987aa27a
omch-29 <chandwaniom29@gmail.com>
2025-09-26 16:09:27 +0530

    added

--- controller/listings.js
+++ controller/listings.js
@@ -24,6 +24,7 @@ module.exports.createListing = async (req, res, next) =>{
     
     const newListing =  new Listing(req.body.listing);
     newListing.image = {url, filename};
+    newListing.owner = req.user_id;
     await newListing.save();
    req.flash("success", "New Listing Created!");
     res.redirect("/listings");
