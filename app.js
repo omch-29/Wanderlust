b4c00323062925f5b65e5b701c152c621b7f0f50
omch-29 <chandwaniom29@gmail.com>
2025-10-18 10:45:28 +0530

    modified

--- app.js
+++ app.js
@@ -130,9 +130,7 @@ app.delete("/listings/:id/reviews/:reviewId",isloggedin,isauthor, wrapAsync(asyn
 )
 )
 
-//  app.all("/*", (req,res,next) =>{
-//      next(new ExpressError(404,"page not found!"));
-//  });
+
 
 app.use((err, req, res, next) =>{
     let {statusCode =500, message="something went wrong"} = err;
