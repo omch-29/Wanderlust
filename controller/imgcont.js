aba9237b3c23000951cf9cc06e1a6e13dd355b84
omch-29 <chandwaniom29@gmail.com>
2026-03-27 11:16:28 +0530

    modified search bar

--- controller/imgcont.js
+++ controller/imgcont.js
@@ -3,7 +3,8 @@ const Listing = require('../models/listing');
 const User = require('../models/user');
 
 async function getImage(req, res){
-    return res.json("sucess").status(200);.
+    const {username, password}=req.body;
+    return res.json("sucess").status(200);
 }
 
 const getImage2 = async(req,res) =>{
