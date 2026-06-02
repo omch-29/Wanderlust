bd9eeb110abaf12b2ec4d19fa05696054d1b5589
omch-29 <chandwaniom29@gmail.com>
2026-06-02 21:50:44 +0530

    schema.js

--- schema.js
+++ schema.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const express = require("express");
-const dotenv = 
+const dotenv = require('dotenv');
 
 module.exports.listingSchema = Joi.object({
     listing: Joi.object({
