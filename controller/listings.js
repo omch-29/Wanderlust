22fd9b0492a526246964679f874eb9bb16cf704c
omch-29 <chandwaniom29@gmail.com>
2026-08-03 19:14:27 +0530

    aaded modification in edit page

--- controller/listings.js
+++ controller/listings.js
@@ -82,7 +82,7 @@ module.exports.updateListing = async (req,res) =>{
 
 module.exports.deleteListing = async (req,res) =>{
     let {id} = req.params;
-    //let {id} = req.params;
+    //
    let listing =await Listing.findById(id);
    if (!listing) {
         req.flash("error", "Listing not found!");
